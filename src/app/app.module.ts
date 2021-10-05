893ac0efd805b7dbd32064b624133058884a074c
harmannawae <you@example.com>
2021-10-06 00:07:14 +0700

    10-6-2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,15 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { AppComponent } from './app.component';
 
 
-import { Camera } from '@ionic-native/camera/ngx'; //ปลักอิน camera จำเป็นต้องติดตั้ง เพิ่ม พิมพว่า $npm install @ionic-native/core@^5.1.0 
-import { Geolocation } from '@ionic-native/geolocation/ngx'; //ปลักอิน googlo location 
+import { Camera } from '@ionic-native/camera/ngx'; //ปลักอิน camera จำเป็นต้องติดตั้ง เพิ่ม พิมพว่า $npm install @ionic-native/core@^5.1.0
+import { Geolocation } from '@ionic-native/geolocation/ngx'; //ปลักอิน googlo location
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
+    BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule,
     IonicStorageModule.forRoot(),
     HttpClientModule
